80b0bc6f430e6e5e11d3ba54f57bc875bc5e19ff
Maricris De Jesus <princetonjudecipriano.com>
2023-07-21 16:47:02 +0800

    test

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Header from "./layouts/Header"
+import Footer from "./layouts/Footer"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import PageContainer from "../layouts/PageContainer"
+
+function Home() {
+  return (
+    <PageContainer>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis,
+        ipsa fugiat nam quo in quam eveniet. Nam omnis, magnam, laudantium
+        molestias iusto maxime asperiores, ducimus dolore voluptatem ad mollitia
+        nesciunt!
+      </p>
+    </PageContainer>
+  )
+}
+
+export default Home

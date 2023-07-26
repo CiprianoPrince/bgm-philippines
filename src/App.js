1e4a3ba94a4013957600d7131c6d368ad367167e
Melvin <melvinxvi@gmail.com>
2023-07-26 16:19:26 +0800

    Adding Services page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Services from './pages/Services'; 
+import Exercise from './pages/Exercise';
 
 function App() {
   return (
-    <></>
+    <div>
+    <Services />
+    <Exercise />
+    
+    </div>
   );
 }
 
